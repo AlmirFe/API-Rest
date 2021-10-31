54c4ebf9e71b185525e661195e705f8ff8343735
Almir Ferreira <almirferreira.sj@gmail.com>
2021-10-31 12:11:34 -0300

    implementando metodos put e delete

--- API/src/main/java/com/almirjr/apirest/controller/ProdutosController.java
+++ API/src/main/java/com/almirjr/apirest/controller/ProdutosController.java
@@ -3,9 +3,11 @@ package com.almirjr.apirest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +36,16 @@ public class ProdutosController {
 	public Produto adicionaProduto (@RequestBody Produto produto) {
 		return ProdutoRepositorio.save(produto);
 		
+	}
+	
+	@DeleteMapping("/produto")
+	public void excluiProduto (@RequestBody Produto produto) {
+		ProdutoRepositorio.delete(produto);
+	}
+	
+	@PutMapping("/produto")
+	public Produto alteraProduto (@RequestBody Produto produto) {
+		return ProdutoRepositorio.save(produto);
 	}
 	
 }
